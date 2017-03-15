423288b4cc8cf1506285913558b3fcff9e7788fa
Vladimir <ReanGD@yandex.ru>
2017-03-15 13:56:43 +0300

    Add URL for monitoring hook

--- gitlab/urls.py
+++ gitlab/urls.py
@@ -6,5 +6,6 @@ from gitlab import views
 urlpatterns = patterns('',
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/app/favicon.ico')),
     url(r'^push_event/hv$', views.push_event_hv),
-    url(r'^push_event/web$', views.push_event_web)
+    url(r'^push_event/web$', views.push_event_web),
+    url(r'^push_event/monitoring$', views.push_event_monitoring),
 )
